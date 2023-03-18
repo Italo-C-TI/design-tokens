63ea5330106616fdfa625732cb9d717aa0a2d494
Italo-C-TI <costa.italo.ti@gmail.com>
2023-03-18 15:35:38 -0300

    teste

---
+++ build/js/tokens.js
@@ -0,0 +1,19 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 13 Mar 2023 00:09:09 GMT
+ */
+
+module.exports = {
+  "ColorBaseGrayLight": "#cccccc",
+  "ColorBaseGrayMedium": "#999999",
+  "ColorBaseGrayDark": "#111111",
+  "ColorBaseRed": "#ff0000",
+  "ColorBaseGreen": "#00ff00",
+  "ColorFontBase": "#ff0000",
+  "ColorFontSecondary": "#00ff00",
+  "ColorFontTertiary": "#cccccc",
+  "SizeFontSmall": "0.75rem",
+  "SizeFontMedium": "1rem",
+  "SizeFontLarge": "2rem",
+  "SizeFontBase": "1rem"
+};
