3ca4f5449f628fdb596d49011e8043fd4eb85c75
Italo-C-TI <costa.italo.ti@gmail.com>
2023-03-20 00:08:16 -0300

    New tokens

---
+++ build/js/tokens.js
@@ -0,0 +1,20 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 20 Mar 2023 03:06:25 GMT
+ */
+
+export const ColorBaseGrayLight = "#cccccc";
+export const ColorBaseGrayMedium = "#999999";
+export const ColorBaseGrayDark = "#111111";
+export const ColorBaseRed = "#ff0000";
+export const ColorBaseGreen = "#00ff00";
+export const RadiiNone = "0";
+export const RadiiSmaller = "4px";
+export const RadiiSmall = "8px";
+export const RadiiMedium = "12px";
+export const RadiiHigh = "16px";
+export const RadiiCircle = "50%";
+export const SizeFontSmall = "0.75rem"; // the small size of the font
+export const SizeFontMedium = "1rem"; // the medium size of the font
+export const SizeFontLarge = "2rem"; // the large size of the font
+export const SizeFontBase = "1rem"; // the base size of the font
